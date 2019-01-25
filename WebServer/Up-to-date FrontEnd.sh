1b2f6417d3324257a92188baffc28ca3d27bb5c7
Matthew Neidig <matthew@neidig.cc>
2019-01-24 22:42:13 -0500

    be a bit more verbose

--- WebServer/Up-to-date FrontEnd.sh
+++ WebServer/Up-to-date FrontEnd.sh
@@ -6,5 +6,7 @@ while true; do
 git clone https://github.com/Xicronic/poosdsmallproj
 sudo rm -rf /var/www/html/*
 sudo cp ./poosdsmallproj/FrontEnd/* /var/www/html/
+rm -rf ./poosdsmallproj/ # Clear out folder, in case a file is deleted in a commit
+echo "Synced, sleeping for 5 minutes"
 sleep 300
 done
