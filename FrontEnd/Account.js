d45958435e1ebcb1683fa5241ad71e10d876cb18
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-01-29 18:13:55 -0500

    WIP js to php communication

--- FrontEnd/Account.js
+++ FrontEnd/Account.js
@@ -1,3 +1,10 @@
+//API Interaction
+var APIRoot = "http://COP4331-3.com/IceCreams/API";
+var fileExtension = ".php";
+var userId = 0;
+var firstName = '', lastName = '';
+var xhttp = new XMLHttpRequest();
+
 //Account Functions
 var users = 0;
 
@@ -7,12 +14,29 @@ function addUser()
     return(users);
 }
 
-function createAccount()
+function createAccount(name,pass)
 {
     var id = addUser();
-    var name = document.getElementById("user").innerHTML;
-    var pass = document.getElementById("pass").innerHTML;
+    //var name = document.getElementById("user").value;
+    //var pass = document.getElementById("pass").value;
     var dateMade = getAccountDate()
+}
+
+function doLogin(x)
+{
+    var name = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+    //(0) means signing into account
+    if(x == 0)
+    {
+        $.post('SearchContacts.php', {variable: name, variable: pass});
+    }
+    //(1) means creating an account
+    else if(x == 1)
+    {
+        $.post('Add.php', {variable: name, variable: pass});
+    }
+    
 }
 
 function getAccountDate()
