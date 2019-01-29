99bb38739ea836866e60477cd1f51953f2c346ec
Matthew Neidig <matthew@neidig.cc>
2019-01-29 17:23:57 -0500

    fix errors involving being in the wrong directory

--- WebServer/check.sh
+++ WebServer/check.sh
@@ -2,13 +2,17 @@
 
 # Run this on the server; checks for updates every ~10 seconds
 
+cd ./poosdsmallproj/
+
 while true; do
 if ! git diff --quiet remotes/origin/HEAD; then
  # Local files NOT up-to-date
+ cd ..
+ rm -rf ./poosdsmallproj/ # Clear out folder, in case a file is deleted in a commit
  git clone https://github.com/Xicronic/poosdsmallproj
  sudo rm -rf /var/www/html/*
  sudo cp ./poosdsmallproj/FrontEnd/* /var/www/html/
- rm -rf ./poosdsmallproj/ # Clear out folder, in case a file is deleted in a commit 
+ cd ./poosdsmallproj/
 fi
 sleep 10 # Re-check in 10 seconds
 done
