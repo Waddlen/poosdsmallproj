c4ad87681fedfd4a3d98134958d3886ae7b74fa4
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-01-30 17:46:09 -0500

    Update DeleteContact.php

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -19,11 +19,6 @@
         }
     }
 
-    function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
