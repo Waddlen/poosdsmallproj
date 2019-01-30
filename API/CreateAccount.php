046ff421da260c533ce13b4f67a717899882466f
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-01-30 17:46:28 -0500

    Update CreateAccount.php

--- API/CreateAccount.php
+++ API/CreateAccount.php
@@ -32,11 +32,6 @@
         }
     }
 
-    	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
