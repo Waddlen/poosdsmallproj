7da451ddbeabcc70a72e87af04e18a42fdde567e
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-01-30 20:05:56 -0500

    Update AddContact.php

--- API/AddContact.php
+++ API/AddContact.php
@@ -5,7 +5,7 @@
 	$Address = $_POST['Address'];
 	$Userid = $_POST['Userid'];
 
-	$conn = new mysqli("52.91.19.201", "poosdAdmin", "DontForgetThis321", "poosdDB");
+	$conn = new mysqli("poosddb.ckbkojoxqly0.us-east-1.rds.amazonraws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
