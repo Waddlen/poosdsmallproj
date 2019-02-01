9eef81e6c11f60cb5f997ec424925203c995be63
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-02-01 16:48:16 -0500

    added contactid field

--- API/AddContact.php
+++ API/AddContact.php
@@ -4,6 +4,7 @@
 	$ContactNumber = $_POST['ContactNumber'];
 	$Address = $_POST['Address'];
 	$Userid = $_POST['Userid'];
+	$Contactid = $_POST['Contactid'];
 
 	$conn = new mysqli("poosddb.ckbkojoxqly0.us-east-1.rds.amazonraws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
 	if ($conn->connect_error) 
@@ -12,7 +13,7 @@
 	} 
 	else
 	{
-		$sql = "INSERT INTO Contact (Userid,ContactName,ContactNumber,Address) VALUES (" . $Userid . ",'" . $ContactName . "','" . $ContactNumber . "','" . $Address . "')";
+		$sql = "INSERT INTO Contact (Contactid,Userid,ContactName,ContactNumber,Address) VALUES (" . $Contactid . "," . $Userid . ",'" . $ContactName . "','" . $ContactNumber . "','" . $Address . "')";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
