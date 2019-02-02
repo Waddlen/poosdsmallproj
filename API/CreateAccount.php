34d0fa17e9b7bfb69befee78578ec9b6548b5816
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-02-01 19:34:44 -0500

    Update CreateAccount.php

--- API/CreateAccount.php
+++ API/CreateAccount.php
@@ -9,7 +9,7 @@
     // $Userid = incrementer++;
     // we would need to store $incrementer somewhere
 
-    $conn = new mysqli("poosddb.ckbkojoxqly0.us-east-1.rds.amazonraws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
+    $conn = new mysqli("poosddb.ckbkojoxq1y0.us-east-1.rds.amazonaws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
