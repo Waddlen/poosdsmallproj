bdbb19bc00fffa61667dd0c5d17d26e0548e3e0f
Matthew Neidig <matthew@neidig.cc>
2019-02-02 12:17:53 -0500

    fix query not running

--- API/CreateAccount.php
+++ API/CreateAccount.php
@@ -29,6 +29,7 @@
             $hash = password_hash($Password, PASSWORD_DEFAULT);
             $timestamp = date("F j, Y \a\t g:ia");
             $sql = "INSERT INTO User (Userid, Username, Password, DateCreated, LastLogin) VALUES ('0', '" . $Username . "','" . $hash . "', '" . $timestamp . "','" . $timestamp . "')";
+            $result = $conn->query($sql);
         }
     }
 
