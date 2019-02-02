f79d9dcd42d238b8a73d9be0549fbf27ad3cf2f8
Matthew Neidig <matthew@neidig.cc>
2019-02-02 14:18:46 -0500

    decode JSON

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -1,14 +1,14 @@
 <?php
-    
-    $conn = new mysqli("poosddb.ckbkojoxq1y0.us-east-1.rds.amazonaws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
-	if ($conn->connect_error) 
+  $_POST = json_decode(file_get_contents('php://input'), true);
+  $conn = new mysqli("poosddb.ckbkojoxq1y0.us-east-1.rds.amazonaws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
         $sql = "DELETE FROM Contact WHERE Userid='" . $_POST['Userid'] . "' AND ContactName='" . $_POST['ContactName'] . "'";
-        if ($conn->query($sql) != TRUE) 
+        if ($conn->query($sql) != TRUE)
         {
             $conn->close();
             returnWithError( "Error deleting contact" );
@@ -24,7 +24,7 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
