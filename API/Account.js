83ae0641a1e6b127d3ff8c73c7fbffb25b9c9182
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-02-02 18:46:35 -0500

    Altered the code to look more like the professor's code, but not sure if it will work or not

--- API/Account.js
+++ API/Account.js
@@ -1,5 +1,5 @@
 //API Interaction
-var APIRoot = "../API";
+var APIRoot = "../API/";
 var fileExtension = ".php";
 var userId = 0;
 
@@ -18,32 +18,114 @@ function createAccount(name,pass)
     var id = addUser();
     var login = document.getElementById("user").value;
     var password = document.getElementById("pass").value;
-//     var dateMade = getAccountDate()
-//     already have this in PHP file
 }
 
-function addContact()
-{
-}
-
+//Login into Account
 function doLogin(x)
 {
+    //alert("Attempt Login");
     var login = document.getElementById("user").value;
     var password = document.getElementById("pass").value;
 
-    //var xhr= new XMLHttpRequest();
+    //alert(login + " " + password);
+    //window.location.href = "contacts.html";
 
+    //Method 1 "JSON" FYI I'm pretty much copying from code.js so I'm not sure how well this will work
+    var jsonPayload = '{"Username" : "' + login + '", "Password" : "' + password + '"}';
+    var url;
+    var xhr= new XMLHttpRequest();
+
+    //Sign In
+    if(x == 0)
+    {
+        url = APIRoot + "Login" + fileExtension;
+        xhr.open("POST", url, false);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+            xhr.send(jsonPayload);
+            
+            var jsonObject = JSON.parse( xhr.responseText );
+            
+            userId = jsonObject.id;
+            
+            if( userId < 1 )
+            {
+                document.getElementById("LogError").innerHTML = "User/Password combination incorrect";
+                return;
+            }
+            /* This Looks like User info for the Contact page
+            firstName = jsonObject.firstName;
+            lastName = jsonObject.lastName;
+
+            document.getElementById("userName").innerHTML = firstName + " " + lastName;
+            
+            document.getElementById("loginName").value = "";
+            document.getElementById("loginPassword").value = "";
+            
+            hideOrShow( "loggedInDiv", true);
+            hideOrShow( "accessUIDiv", true);
+            hideOrShow( "loginDiv", false);
+            */
+        }
+        catch(err)
+        {
+            document.getElementById("LogError").innerHTML = err.message;
+        }
+    }
+    //Create Account
+    else if(x == 1)
+    {
+        url = APIRoot + "CreateAccount" + fileExtension;
+        xhr.open("POST", url, false);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+            xhr.send(jsonPayload);
+            
+            var jsonObject = JSON.parse( xhr.responseText );
+            
+            userId = jsonObject.id;
+            
+            if( userId < 1 )
+            {
+                document.getElementById("LogError").innerHTML = "Account Info is incorrect or already taken";
+                return;
+            }
+            /* This Looks like User info for the Contact page
+            firstName = jsonObject.firstName;
+            lastName = jsonObject.lastName;
+
+            document.getElementById("userName").innerHTML = firstName + " " + lastName;
+            
+            document.getElementById("loginName").value = "";
+            document.getElementById("loginPassword").value = "";
+            
+            hideOrShow( "loggedInDiv", true);
+            hideOrShow( "accessUIDiv", true);
+            hideOrShow( "loginDiv", false);
+            */
+        }
+        catch(err)
+        {
+            document.getElementById("LogError").innerHTML = err.message;
+        }
+    }
+
+    /*Method 2 "Post"
     //(0) means signing into account
     if(x == 0)
     {
         //should access Login.php and post values to them. May need to use: $name = $_Post['Username']; to get value
+        
+        
         $.post('Login.php', {Username: login, Password: password},
         function(data)
         {
             //if .php states: echo "0"; all is good
             if(data == "0")
             {
-                window.location.href = "contacts.html"
+                window.location.href = "contacts.html";
             }
             //Wrong password
             else if(data == "1")
@@ -56,8 +138,6 @@ function doLogin(x)
                 document.getElementById("LogError").innerHTML = "This account does not exist";
             }
         });
-        //xhr.open("GET", "Login.php", true);
-        //xhr.send();
     }
     //(1) means creating an account
     else if(x == 1)
@@ -69,7 +149,7 @@ function doLogin(x)
             //if .php states: echo "0"; all is good
             if(data == "0")
             {
-                window.location.href = "contacts.html"
+                window.location.href = "contacts.html";
             }
             //Username is already being used
             else
@@ -78,18 +158,5 @@ function doLogin(x)
             }
         });
     }
-    
-    //are these .php files referring to, what? shouldn't doLogin refer to
-    //Login.php or CreateAccount.php? Why is it referring instead to these?
-    
+    */
 }
-
-// function getAccountDate()
-// {
-//     var d = new Date();
-//     var m = d.getMonth();
-//     var t = d.getDay();
-//     var y = d.getFullYear();
-//     //alert(m + " " + t + ", " + y);
-//     return(m + " " + t + ", " + y);
-// }
