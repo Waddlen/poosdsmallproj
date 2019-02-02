8cd02e4a6ab0faac57c18356db9653204a730d11
Matthew Neidig <matthew@neidig.cc>
2019-02-02 12:33:36 -0500

    AddContact API fixes

--- API/AddContact.php
+++ API/AddContact.php
@@ -1,11 +1,11 @@
 <?php
 	$_POST = json_decode(file_get_contents('php://input'), true);
-	$ContactFirstName = $_POST['ContactName'];
+	$ContactFirstName = $_POST['ContactFirstName'];
 	$ContactLastName = $_POST['ContactLastName'];
 	$ContactNumber = $_POST['ContactNumber'];
 	$Address = $_POST['Address'];
 	$Userid = $_POST['Userid'];
-	$Contactid = $_POST['Contactid'];
+	//$Contactid = $_POST['Contactid']; //This field is auto-incremented by DB
 
 	$conn = new mysqli("poosddb.ckbkojoxq1y0.us-east-1.rds.amazonaws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
 	if ($conn->connect_error)
@@ -14,7 +14,7 @@
 	}
 	else
 	{
-		$sql = "INSERT INTO Contact (Contactid,Userid,ContactFirstName,ContactLastName,ContactNumber,Address) VALUES (" . $Contactid . "," . $Userid . ",'" . $ContactFirstName . "','" . $ContactLastName . "','" . $ContactNumber . "','" . $Address . "')";
+		$sql = "INSERT INTO Contact (Contactid,Userid,ContactFirstName,ContactLastName,ContactNumber,Address) VALUES ('0','" . $Userid . "','" . $ContactFirstName . "','" . $ContactLastName . "','" . $ContactNumber . "','" . $Address . "')";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
