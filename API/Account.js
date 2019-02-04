09e55982b690e8a0c9285dea5402d1ccacef18b0
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-02-04 01:55:19 -0500

    Update Account.js

--- API/Account.js
+++ API/Account.js
@@ -62,7 +62,7 @@ function searchContacts()
                         newContactinfo.insertCell(4).outerHTML = '<th scope="col">'+jsonObjectTwo.Address+"</th>";
                         var Contactid = jsonObjectTwo.Contactid;
                         newContactinfo.insertCell(5).outerHTML = '<th scope="col"><button type="button" onclick="setUpdateId(Contactid)" class="btn btn-primary btn" data-toggle="modal" data-target="#EditContactModal">Edit</button></th>';
-                        newContactinfo.insertCell(6).outerHTML = '<th scope="col"><button type="button" class="btn btn-primary btn" onclick="deleteThis(this)">Delete</button></th>';
+                        newContactinfo.insertCell(6).outerHTML = '<th scope="col"><button type="button" class="btn btn-primary btn" onclick="deleteThis(this, Contactid)">Delete</button></th>';
                         //var newRow = table.rows[0];
                         //table.parent.insertBefore(newRow, table.rows[1]);
                         //alert(ContactName);
