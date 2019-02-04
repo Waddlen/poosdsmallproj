5e3abc7a8195d82ea57379508bf4650ef17d13c4
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-02-04 00:23:53 -0500

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -42,7 +42,7 @@
 				$conn->close();
 
 				//echo "0";
-				returnWithInfo($Username, $Userid );
+				returnWithInfo($Username, $Userid, $DateCreated, $LastLogin );
 			}
 		}
 		else
@@ -66,7 +66,7 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-	function returnWithInfo( $Username, $Userid )
+	function returnWithInfo( $Username, $Userid, $DateCreated, $LastLogin )
 	{
 		$retValue = '{"Username":"' . $Username . '","DateCreated":"' . $DateCreated . '","LastLogin":"' . $LastLogin . '","Userid":"' . $Userid . '","error":""}';
 		sendResultInfoAsJson( $retValue );
