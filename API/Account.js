8a365c94e6442c2a8aeb9711fe1814975d9b5760
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-02-04 02:07:43 -0500

    Update Account.js

--- API/Account.js
+++ API/Account.js
@@ -56,7 +56,7 @@ function searchContacts()
                         newContactinfo.scope = "row";
                         newContactinfo.value = "1";
                         newContactinfo.insertCell(0).outerHTML = '<th scope="col">'+(jsonObject.results.length - i)+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
-                        newContactinfo.insertCell(1).outerHTML = '<th scope="col">'+jsonObjectTwo.ContactFirstName+"</th>";
+                        newContactinfo.insertCell(1).outerHTML = '<th scope="col">'+jsonObjectTwo.ContactFirstName+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
                         newContactinfo.insertCell(2).outerHTML = '<th scope="col">'+jsonObjectTwo.ContactLastName+"</th>";
                         newContactinfo.insertCell(3).outerHTML = '<th scope="col">'+jsonObjectTwo.ContactNumber+"</th>";
                         newContactinfo.insertCell(4).outerHTML = '<th scope="col">'+jsonObjectTwo.Address+"</th>";
